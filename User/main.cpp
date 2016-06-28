4208dd6686476ca4cf50fd98640ec2e0bcaa7ff4
quanmao <cat_li8953@qq.com>
2016-06-28 17:34:00 +0800

    添加rx,tx中断测试

--- User/main.cpp
+++ User/main.cpp
@@ -11,41 +11,39 @@ Copyright 2015 shentq. All Rights Reserved.
 #include "stm32f0xx_hal.h"
 
 #include "ebox.h"
-void rx_even(void)
+
+void rx_event()
 {
-	uint8_t c;
-	c = uart2.receive();
-	uart2.put_char(c);
+char c;
+c = uart2.receive();
+uart2.put_char(c);
 }
+
+void tx_event()
+{
+PA5.toggle();
+}
+
 
 void setup()
 {
-    ebox_init();
-	  PA5.mode(OUTPUT_PP);
-	  uart2.begin(115200);
-	  uart2.printf("test66666666"); 
-	  uart2.attach_rx_interrupt(rx_even);
-    	
+	ebox_init();
+	PA5.mode(OUTPUT_PP);
+	uart2.begin(115200);
+	uart2.printf("test66666666");
+//	uart2.attach_rx_interrupt(rx_event);
+//	uart2.attach_tx_interrupt(tx_event);
+
 }
 int main(void)
 {
-    setup();
-    //random_seed(10);
-    while(1)
-    {
-			uart2.printf("test66666666");
-			PA5.toggle();
-			delay_ms(500);
-//        PA15.toggle();
-//        for(int i = 0; i < 10;i++)
-//        {
-//            wbuf[i] = i;
-//        }
-//        flash.write_sector(ADDR_FLASH_SECTOR_4,wbuf,10);
-//        flash.read(ADDR_FLASH_SECTOR_4,rbuf,10);
-
-//        delay_ms(100);
-    }
+	setup();
+	//random_seed(10);
+	while (1)
+	{
+		uart2.printf("test66666666");
+		delay_ms(500);
+	}
 }
 
 
