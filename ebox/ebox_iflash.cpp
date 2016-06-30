84b0269128a80ab921c1e31e30cfc0fa7037b581
quanmao <cat_li8953@qq.com>
2016-06-30 17:37:00 +0800

    移植到STM32F0

--- ebox/ebox_iflash.cpp
+++ ebox/ebox_iflash.cpp
@@ -3,7 +3,7 @@ file   : iflash.cpp
 author : shentq
 version: V1.1
 date   : 2015/7/5
-
+date   : 2016/6/30 LQM移植到STM32F0平台
 Copyright 2015 shentq. All Rights Reserved.
 
 Copyright Notice
@@ -17,47 +17,57 @@ This specification is preliminary and is subject to change at any time without n
 
 
 
-
-
+/**
+ *@name     int erase_sector(FLASH_SECTOR_ADDR_t start_addr)
+ *@brief    F0系列Flash实际是按页排列的，所以这里实际是檫除1页
+ *@param    start_addr：  要檫除的页地址
+ *@retval   NONE
+*/
 int FLASHCLASS::erase_sector(FLASH_SECTOR_ADDR_t start_addr)
 {
-    FLASH_Unlock();
-      /* Clear pending flags (if any) */  
-    FLASH_ClearFlag(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | 
-                  FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR|FLASH_FLAG_PGSERR); 
+	/*Variable used for Erase procedure*/
+	static FLASH_EraseInitTypeDef EraseInitStruct;
+	uint32_t PageError = 0;
 
-    
-    if (FLASH_EraseSector(addr_to_sector(start_addr), VoltageRange_3) != FLASH_COMPLETE)
-    { 
-      /* Error occurred while sector erase. 
-         User can add here some code to deal with this error  */
-      while (1)
-      {
-      }
-    }
-    FLASH_Lock();
+	HAL_FLASH_Unlock();
+	/* Clear pending flags (if any) */
+	// FLASH_ClearFlag(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR |
+	// FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR|FLASH_FLAG_PGSERR);
+	/* Fill EraseInit structure*/
+	EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
+	EraseInitStruct.PageAddress = start_addr;
+	EraseInitStruct.NbPages = 1;
 
+	if (HAL_FLASHEx_Erase(&EraseInitStruct, &PageError) != HAL_OK)
+	{
+		/* Error occurred while sector erase.
+		   User can add here some code to deal with this error  */
+		while (1)
+		{
+		}
+	}
+	HAL_FLASH_Lock();
 }
 
 int FLASHCLASS::write_sector(FLASH_SECTOR_ADDR_t start_addr, uint8_t *buf, uint32_t num_to_write)
 {
 
-    FLASH_Unlock();
+    HAL_FLASH_Unlock();
        /* Clear pending flags (if any) */  
-    FLASH_ClearFlag(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | 
-                  FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR|FLASH_FLAG_PGSERR); 
+    // FLASH_ClearFlag(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | 
+                  // FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR|FLASH_FLAG_PGSERR); 
 
     
-    if (FLASH_EraseSector(addr_to_sector(start_addr), VoltageRange_3) != FLASH_COMPLETE)
-    { 
-      /* Error occurred while sector erase. 
-         User can add here some code to deal with this error  */
-      while (1)
-      {
-      }
-    }
-   write_without_check(start_addr,buf,num_to_write);    
-    FLASH_Lock();
+    // if (FLASH_EraseSector(addr_to_sector(start_addr), VoltageRange_3) != FLASH_COMPLETE)
+    // { 
+      // /* Error occurred while sector erase. 
+         // User can add here some code to deal with this error  */
+      // while (1)
+      // {
+      // }
+    // }
+    write_without_check(start_addr,buf,num_to_write);    
+    HAL_FLASH_Lock();
     return num_to_write;
 }
 int FLASHCLASS::read(uint32_t Address, uint8_t *buf, int32_t num_to_read)
@@ -69,74 +79,82 @@ int FLASHCLASS::read(uint32_t Address, uint8_t *buf, int32_t num_to_read)
         i+=1;
     }
     return i;
-
 }
 
 
 uint16_t FLASHCLASS::addr_to_sector(uint32_t Address)
 {
   
-  if((Address < ADDR_FLASH_SECTOR_1) && (Address >= ADDR_FLASH_SECTOR_0))
-  {
-    return FLASH_Sector_0;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_2) && (Address >= ADDR_FLASH_SECTOR_1))
-  {
-    return FLASH_Sector_1;
+  // if((Address < ADDR_FLASH_SECTOR_1) && (Address >= ADDR_FLASH_SECTOR_0))
+  // {
+    // return FLASH_Sector_0;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_2) && (Address >= ADDR_FLASH_SECTOR_1))
+  // {
+    // return FLASH_Sector_1;
 
-  }
-  else if((Address < ADDR_FLASH_SECTOR_3) && (Address >= ADDR_FLASH_SECTOR_2))
-  {
-    return FLASH_Sector_2;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_4) && (Address >= ADDR_FLASH_SECTOR_3))
-  {
-    return FLASH_Sector_3;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_5) && (Address >= ADDR_FLASH_SECTOR_4))
-  {
-    return FLASH_Sector_4;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_6) && (Address >= ADDR_FLASH_SECTOR_5))
-  {
-    return FLASH_Sector_5;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_7) && (Address >= ADDR_FLASH_SECTOR_6))
-  {
-    return FLASH_Sector_6;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_8) && (Address >= ADDR_FLASH_SECTOR_7))
-  {
-    return FLASH_Sector_7;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_9) && (Address >= ADDR_FLASH_SECTOR_8))
-  {
-    return FLASH_Sector_8;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_10) && (Address >= ADDR_FLASH_SECTOR_9))
-  {
-    return FLASH_Sector_9;
-  }
-  else if((Address < ADDR_FLASH_SECTOR_11) && (Address >= ADDR_FLASH_SECTOR_10))
-  {
-    return FLASH_Sector_10;
-  }
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_3) && (Address >= ADDR_FLASH_SECTOR_2))
+  // {
+    // return FLASH_Sector_2;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_4) && (Address >= ADDR_FLASH_SECTOR_3))
+  // {
+    // return FLASH_Sector_3;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_5) && (Address >= ADDR_FLASH_SECTOR_4))
+  // {
+    // return FLASH_Sector_4;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_6) && (Address >= ADDR_FLASH_SECTOR_5))
+  // {
+    // return FLASH_Sector_5;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_7) && (Address >= ADDR_FLASH_SECTOR_6))
+  // {
+    // return FLASH_Sector_6;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_8) && (Address >= ADDR_FLASH_SECTOR_7))
+  // {
+    // return FLASH_Sector_7;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_9) && (Address >= ADDR_FLASH_SECTOR_8))
+  // {
+    // return FLASH_Sector_8;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_10) && (Address >= ADDR_FLASH_SECTOR_9))
+  // {
+    // return FLASH_Sector_9;
+  // }
+  // else if((Address < ADDR_FLASH_SECTOR_11) && (Address >= ADDR_FLASH_SECTOR_10))
+  // {
+    // return FLASH_Sector_10;
+  // }
 }
 
+/**
+ *@name     uint16_t write_without_check(uint32_t iAddress, uint8_t *buf, uint16_t iNumByteToWrite)
+ *@brief    F0系列Flash实际是按页排列的，所以这里实际是檫除1页
+ *@param    iAddress  要檫除的页地址
+	        *buf 	  指向要写入的数据缓冲区
+			iNumByteToWrite 要写入多少数据
+ *@retval   NONE
+*/
 uint16_t FLASHCLASS::write_without_check(uint32_t iAddress, uint8_t *buf, uint16_t iNumByteToWrite)
 {
     uint16_t i;
-    volatile FLASH_Status FLASHStatus = FLASH_COMPLETE;
+	uint32_t dat;
+    volatile HAL_StatusTypeDef FLASHStatus = HAL_OK;
     i = 0;
-
     //    FLASH_UnlockBank1();
-    while((i < iNumByteToWrite) && (FLASHStatus == FLASH_COMPLETE))
+    while((i < iNumByteToWrite) && (FLASHStatus == HAL_OK))
     {
-        FLASHStatus = FLASH_ProgramWord(iAddress, *(uint32_t *)buf);
+		dat = *(uint32_t *)buf;
+        //FLASHStatus = FLASH_ProgramWord(iAddress, *(uint32_t *)buf);
+		FLASHStatus = HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD,iAddress, (uint8_t) *buf);
         i = i + 4;
-        iAddress = iAddress + 4;
-        buf = buf + 4;
+        iAddress = iAddress + 2;
+        buf = buf + 1;
     }
-
     return iNumByteToWrite;
 }
